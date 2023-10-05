5bdf11ec357c9ff72a09049a5e0c2c7f3943997f
Visinho <Elvisvisinho@rocketmail.com>
2023-10-05 14:57:50 +0100

    refactored the endpoints to use controllers and services

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,17 @@
-import { BadRequestException, NotFoundException, Controller, Body, Get, Param, Post, Put, Delete, HttpCode } from '@nestjs/common';
+import {
+  BadRequestException,
+  NotFoundException,
+  Controller,
+  Body,
+  Get,
+  Param,
+  Post,
+  Put,
+  Delete,
+  HttpCode,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { data, ReportType } from './data';
-import { v4 as uuid } from 'uuid';
 
 @Controller('report/:type')
 export class AppController {
@@ -9,107 +19,54 @@ export class AppController {
 
   @Get()
   getAllReports(@Param('type') type: string) {
-    if (type !== 'income' && type !== 'expense') {
-      throw new BadRequestException(
-        'Invalid report type. Accepted types are: income, expense',
-      );
-    }
     const reportType =
       type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    return data.report.filter((report) => report.type === reportType);
+    return this.appService.getAllReports(reportType);
   }
 
   @Get(':id')
   getReportById(@Param('type') type: string, @Param('id') id: string) {
-  
-      if (type !== 'income' && type !== 'expense') {
-          throw new BadRequestException('Invalid report type. Accepted types are: income, expense');
-      }
-  
-      const reportType = type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-  
-      const report = data.report
-          .filter((report) => report.type === reportType)
-          .find((report) => report.id === id);
-  
-      if (!report) {
-          throw new NotFoundException(`Report with id ${id} not found.`);
-      }
-  
-      return report;
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    return this.appService.getReportById(reportType, id);
   }
-  
+
   @Post()
   createReport(
-    @Body() { amount, source }: {
-      amount: number
-      source: string
-    }, @Param('type') type: string
-  ) {
-    if (type !== 'income' && type !== 'expense') {
-      throw new BadRequestException(
-        'Invalid report type. Accepted types are: income, expense',
-      );
-    }
-    const newReport = {
-      id: uuid(),
+    @Body()
+    {
       amount,
       source,
-      created_at: new Date(),
-      updated_at: new Date(),
-      type: type === "income" ? ReportType.INCOME : ReportType.EXPENSE
-    }
-    data.report.push(newReport)
-    return newReport;
-  }
-  
-  @Put(":id")
-  updateReport(
-    @Param("type") type: string,
-    @Param('id') id: string,
-    @Body() body: {
-      amount: number
-      source: string
-    }
+    }: {
+      amount: number;
+      source: string;
+    },
+    @Param('type') type: string,
   ) {
-    const reportType = type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    const reportToUpdate = data.report
-    .filter((report) => report.type === reportType)
-    .find((report) => report.id === id);
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.appService.createReport(reportType, {amount, source});
+  }
 
-if (!reportToUpdate) {
-    throw new NotFoundException(`Report with id ${id} not found.`);
-}
-const reportIndex = data.report.findIndex((report) => report.id === reportToUpdate.id)
+  @Put(':id')
+  updateReport(
+    @Param('type') type: string,
+    @Param('id') id: string,
+    @Body()
+    body: {
+      amount: number;
+      source: string;
+    },
+  ) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.appService.updateReport(reportType, id, body)
+  }
 
-data.report[reportIndex] = {
-  ...data.report[reportIndex],
-  ...body,
-}
-      return data.report[reportIndex] 
-}
-  
-@HttpCode(204)
-@Delete(":id")
-deleteReport(
-  @Param("id") id: string,
-  @Param("type") type: string,
-){
-  
-  const reportType = type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    const reportToDelete = data.report
-    .filter((report) => report.type === reportType)
-    .find((report) => report.id === id);
-  const reportIndex = data.report.findIndex((report) => report.id === reportToDelete.id);
-
-  if (!reportToDelete) {
-    throw new NotFoundException(`Report with id ${id} not found.`);
-}
-
-data.report.splice(reportIndex, 1);
-return "Report deleted successfully";
-}
-  
-
-
+  @HttpCode(204)
+  @Delete(':id')
+  deleteReport(@Param('id') id: string) {
+    return this.appService.deleteReport(id);
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,95 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
+import { ReportType, data } from './data';
+import { v4 as uuid } from 'uuid';
+
+
+interface DataProps {
+  amount: number
+  source: string
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getAllReports(type: ReportType) {
+    if (type !== 'income' && type !== 'expense') {
+      throw new BadRequestException(
+        'Invalid report type. Accepted types are: income, expense',
+      );
+    }
+    return data.report.filter((report) => report.type === type);
+  }
+
+  getReportById(type: ReportType, id: string) {
+    if (type !== 'income' && type !== 'expense') {
+      throw new BadRequestException(
+        'Invalid report type. Accepted types are: income, expense',
+      );
+    }
+
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    const report = data.report
+      .filter((report) => report.type === type)
+      .find((report) => report.id === id);
+
+    if (!report) {
+      throw new NotFoundException(`Report with id ${id} not found.`);
+    }
+    return report;
+  }
+
+  createReport(type: ReportType, { amount, source} : DataProps){
+    if (type !== 'income' && type !== 'expense') {
+      throw new BadRequestException(
+        'Invalid report type. Accepted types are: income, expense',
+      );
+    }
+    const newReport = {
+      id: uuid(),
+      amount,
+      source,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type
+    };
+    data.report.push(newReport);
+    return newReport;
+  }
+
+  updateReport(type: ReportType, id: string, body : DataProps){
+    const reportToUpdate = data.report
+      .filter((report) => report.type === type)
+      .find((report) => report.id === id);
+
+    if (!reportToUpdate) {
+      throw new NotFoundException(`Report with id ${id} not found.`);
+    }
+    const reportIndex = data.report.findIndex(
+      (report) => report.id === reportToUpdate.id,
+    ); 
+
+    data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      ...body,
+      updated_at: new Date()
+    };
+    return data.report[reportIndex];
+  }
+
+  deleteReport(id: string){
+
+   const reportIndex = data.report.findIndex((report) => report.id === id);
+
+    if (reportIndex !== -1) {
+      data.report.splice(reportIndex, 1);
+    } else {
+      throw new NotFoundException(`Report with id ${id} not found.`);
+    }
+      
   }
 }
