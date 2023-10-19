c7677e670311e8cac6bb56d70669f8df7fd2ca85
Visinho <Elvisvisinho@rocketmail.com>
2023-10-19 09:58:38 +0100

    Added the Summary endpoint

--- src/summary/summary.service.ts
+++ src/summary/summary.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ReportType } from 'src/data';
 import { ReportService } from 'src/report/report.service';
 
 @Injectable()
 export class SummaryService {
+  constructor(private readonly reportService: ReportService) {}
+  calculateSummary() {
+    const totalExpense = this.reportService.getAllReports(ReportType.EXPENSE).reduce((sum, report) => sum + report.amount, 0);
+    const totalIncome = this.reportService.getAllReports(ReportType.INCOME).reduce((sum, report) => sum + report.amount, 0);
 
-    constructor(private readonly reportService: ReportService){}
-    calculateSummary() {
-
+    return {
+        totalIncome,
+        totalExpense,
+        netIncome: totalIncome - totalExpense,
     }
+  }
 }
